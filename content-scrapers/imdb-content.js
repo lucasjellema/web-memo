b58c283e0e0738d81cd714d02257b58630e0dcd0
Lucas Jellema <lucasjellema@gmail.com>
2025-02-16 12:36:58 +0100

    add more like this and known for - imdb

--- content-scrapers/imdb-content.js
+++ content-scrapers/imdb-content.js
@@ -50,6 +50,8 @@ const getIMDbProfile = () => {
   const profile = {}
   addName(profile)
 
+  titlePage = window.location.href.includes('/title/')
+  namePage = window.location.href.includes('/name/')
   if (titlePage) {
     addType(profile)
     addRating(profile)
@@ -57,6 +59,7 @@ const getIMDbProfile = () => {
     addPeriod(profile)
     addChipsAndAbout(profile)
     addCast(profile)
+    addMoreLikeThis(profile)
   }
   if (namePage) {
     addImage(profile)
@@ -64,6 +67,7 @@ const getIMDbProfile = () => {
     addBio(profile)
     addPersonSubtype(profile)
     addPortfolio(profile)
+    addKnownFor(profile)
 
   }
   return profile
@@ -156,7 +160,6 @@ const addImage = (profile) => {
 
 const addChipsAndAbout = (profile) => {
   let chipDiv = document.querySelector("div.ipc-chip-list");
-  console.log('chipDiv', chipDiv)
   if (chipDiv) {
     const chips = chipDiv.querySelector('div:nth-of-type(2)').querySelectorAll('a');
     if (chips) {
@@ -164,14 +167,12 @@ const addChipsAndAbout = (profile) => {
     }
   }
   let plotP = chipDiv.nextElementSibling
-  console.log('plotP', plotP)
   if (plotP) {
     profile.plot = plotP.querySelector('span:nth-of-type(2)').textContent.trim();
   }
   let detailsDiv = plotP.nextElementSibling
   if (detailsDiv) {
     const detailLines = detailsDiv.querySelector('ul').querySelectorAll(':scope >li');
-    console.log('detailLines', detailLines)
     if (detailLines) {
       for (let i = 0; i < detailLines.length; i++) {
         const detail = detailLines[i];
@@ -198,15 +199,50 @@ const addChipsAndAbout = (profile) => {
   }
 }
 
+const addKnownFor = (profile) => {
+  // find div with data-testid="nm_kwn_for_0"
+  const knownForDiv = document.querySelector('div[data-testid="nm_kwn_for_0"]');
+  if (knownForDiv) {
+    const parentKnownForDiv = knownForDiv.parentElement
+    if (parentKnownForDiv) {
+      const knownFor = []
+      for (const child of parentKnownForDiv.children) {
+        const production = {}
+        production.image = child.querySelector('img').src
+        production.movieUrl = child.querySelector('a').href
+        production.description = child.querySelector('a').ariaLabel
+        production.rating = child.querySelector('span.ipc-rating-star--rating').textContent
+        production.type = child.querySelector('div.ipc-primary-image-list-card__title-type')?.textContent
 
+        const div = child.querySelector(':scope > div:nth-of-type(2)')
+        if (div) {
+          const title = div.querySelector(':scope > div:nth-of-type(1)')
+          if (title) {
+            production.title = title.textContent.trim()
+          }
+          const character = div.querySelector(':scope > div:nth-of-type(3)')
+          if (character) {
+            production.character = character.textContent.trim()
+          }
+          const period = div.querySelector(':scope > div:nth-of-type(4)')
+          if (period) {
+            production.period = period.textContent.trim()
+          }
+        }
+
+        knownFor.push(production)
+      }
+      profile.knownFor = knownFor
+    }
+  }
+}
 
 
 const addPortfolio = (profile) => {
   let filmographyDiv = document.querySelector('div[data-testid="Filmography"]');
   if (filmographyDiv) {
-    console.log('filmographyDiv', filmographyDiv)
+
     const portfolioDiv = filmographyDiv.querySelector(':scope > section:nth-of-type(2)').querySelector(':scope > div:nth-of-type(5)')
-    console.log('portfolioDiv', portfolioDiv)
     if (portfolioDiv) {
       // const portfolioUl = portfolioDiv.querySelector('ul:nth-of-type(2)');
       // console.log('portfolioUl', portfolioUl)
@@ -221,11 +257,9 @@ const addPortfolio = (profile) => {
       const perspectives = ['actor', 'actress', 'director', 'writer', 'producer']
       for (const perspective of perspectives) {
         const portfolioUl = document.getElementById(`accordion-item-${perspective}-previous-projects`)?.querySelector('ul');
-        console.log('portfolioUl', portfolioUl)
 
         if (portfolioUl) {
           const items = portfolioUl.querySelectorAll(':scope > li');
-          console.log('items', items)
           for (let i = 0; i < items.length; i++) {
             const item = items[i];
             const performance = { perspective }
@@ -253,7 +287,6 @@ const addPortfolio = (profile) => {
             }
 
             portfolio.push(performance)
-            console.log('performance', JSON.stringify(performance))
           }
         }
       }
@@ -262,16 +295,48 @@ const addPortfolio = (profile) => {
   }
 }
 
+const addMoreLikeThis = (profile) => {
+  // section data-testid="MoreLikeThis"
+
+  const moreLikeThisSection = document.querySelector('section[data-testid="MoreLikeThis"]');
+  if (moreLikeThisSection) {
+    const moreLikeThisDiv = moreLikeThisSection.querySelector('div[data-testid="shoveler-items-container"]');
+    if (moreLikeThisDiv) {
+      const moreLikeThis = []
+      const items = moreLikeThisDiv.querySelectorAll(':scope > div');
+      if (items) {
+        for (const item of items) {
+          const moreLikeThisItem = {}
+          const image = item.querySelector('img')
+          if (image) {
+            moreLikeThisItem.image = image.src
+            moreLikeThisItem.title = image.alt
+          }
+          const ref = item.querySelector('a')
+          if (ref) {
+            moreLikeThisItem.titleUrl = ref.href
+          }
+          // span with class ipc-rating-star--rating 
+          const rating = item.querySelector('span.ipc-rating-star--rating')
+          if (rating) {
+            moreLikeThisItem.rating = rating.textContent
+          }
+
+          moreLikeThis.push(moreLikeThisItem)
+        }
+        profile.moreLikeThis = moreLikeThis
+      }
+    }
+  }
+}
+
 const addCast = (profile) => {
   let castDiv = document.querySelector('section[data-testid="title-cast"]');
-  console.log('castDiv', castDiv)
   if (castDiv) {
     const cast = castDiv.querySelector('div:nth-of-type(2)').querySelector('div:nth-of-type(2)').querySelectorAll(':scope > div');
-    console.log('cast', cast)
     if (cast) {
       profile.cast = []
       for (let i = 0; i < cast.length; i++) {
-        console.log('cast', cast[i])
         const character = {}
         const el = cast[i]
         const image = el.querySelector('img')
@@ -299,7 +364,6 @@ const addCast = (profile) => {
 const addRating = (profile) => {
   let starIcon = document.querySelector("svg.ipc-icon--star");
   if (starIcon) {
-    console.log('starIcon', starIcon)
     const ratingValue = starIcon.parentElement.nextElementSibling.textContent.trim();
     if (ratingValue) {
       profile.rating = ratingValue;
@@ -314,11 +378,9 @@ const addBio = (profile) => {
     if (grandparent) {
       const nextElementSibling = grandparent.nextElementSibling
       const divWithBio = nextElementSibling.querySelector(':scope > div:nth-of-type(2)')
-      console.log('divWithBio', divWithBio)
       if (divWithBio) {
         const bio = divWithBio.querySelector('section').querySelector(':scope > div').querySelector(':scope > div').querySelector(':scope > div').querySelector(':scope > div').textContent
         profile.bio = bio
-        console.log('bio', bio)
       }
     }
   }
--- processImdbProfile.js
+++ processImdbProfile.js
@@ -1,106 +1,89 @@
+import { generateGUID } from './utils.js';
 
 export const processImdbProfile = ( message) => {
     console.log('imdb profile',message)
-    // const contentDiv = document.getElementById('content');
-    // contentDiv.textContent = `
-    //       Profile Type: ${JSON.stringify(message.profile.type)} \n
-    //       Profile: ${JSON.stringify(message.profile)}
-    //       IMDB URL: ${message.imdbUrl}
-    //     `;
 
-    // let newNodes = cy.collection();
-    // const profile = message.profile;
-    // if (profile.type === 'name') {
-    //     let nameNode = findNodeByProperty(cy, 'label', profile.name);
-    //     if (!nameNode) {
-    //         nameNode = createNode(cy, profile.name);
-    //         nameNode.data('url', message.imdbUrl);
-    //         nameNode.data('type', profile.type);
-    //         nameNode.data('subtype', `imdb${profile.subtype}`);
-    //         nameNode.data('shape', 'star');
-    //         newNodes = newNodes.union(nameNode);
-    //     }
-    //     if (profile.image) nameNode.data('image', profile.image);
-    //     if (profile.bio) nameNode.data('bio', profile.bio);
-    //     if (profile.birthDate) nameNode.data('birthDate', profile.birthDate);
-    //     if (profile.deathDate) nameNode.data('deathDate', profile.deathDate);
 
-    //     if (profile.portfolio) {
-    //         profile.portfolio.forEach(activity => {
-    //             let titleNode = findNodeByProperty(cy, 'label', activity.title);
-    //             if (!titleNode) {
-    //                 titleNode = createNode(cy, activity.title);
-    //                 titleNode.data('url', activity.url);
-    //                 titleNode.data('type', 'title');
-    //                 titleNode.data('subtype', `actor`);
-    //                 titleNode.data('shape', 'square');
-    //                 newNodes = newNodes.union(titleNode);
-    //             }
-    //             if (activity.image) titleNode.data('image', activity.image);
-    //             if (activity.details) titleNode.data('year', activity.details[0]);
-         
-    //             const edge = createEdge(cy, nameNode, titleNode);
-    //             edge.data('label', activity.perspective);
-    //             edge.data('type', activity.perspective);
-    //             edge.data('character', activity.character[0]);
-                
-    //         });
-    //     }
-    // }
+    const profile = message.profile
+    console.log("processIMBdProfile", message)
+    const newNode = {}
+    newNode.id = generateGUID();
+    newNode.name = profile.name;
+    newNode.url = message.imdbUrl;
+    newNode.pageUrl = profile.pageUrl;
+    newNode.pageTitle = profile.pageTitle;
+    newNode.type = profile.type === 'name' ? 'person' : 'movie'
+    newNode.subtype = profile.type
+    if (profile.type === 'name') {
+        newNode.biography = profile.bio;
+        newNode.birthDate = profile.birthDate;
+        newNode.deathDate = profile.deathDate;
+        newNode.image = profile.image;
+        newNode.portfolio = profile.portfolio;
+        newNode.knownFor = profile.knownFor;
+        if (profile.knownFor ) { newNode.knownForString = profile.knownFor.map(item => item.character + ' in ' + item.title + "("+item.period+")").join(', ') }
+    }
+    if (profile.subtype === 'TV Series' || profile.subtype === 'TV Mini Series' || profile.subtype === 'Movie') {
+        if (profile.subtype === 'TV Series' || profile.subtype === 'TV Mini Series') newNode.type='tv'
+        if (profile.subtype === 'Movie' ) newNode.type='movie'
+        
+        newNode.image = profile.image;
+        newNode.plot = profile.plot;
+        newNode.rating = profile.rating.split('/')[0];
+        newNode.chips = profile.chips;
+        if (profile.chips) {
+            // if chips is an array, then add all entries to newNode.tags
+            if (profile.chips && Array.isArray(profile.chips)) {
+                newNode.tags = profile.chips?.map(tag => tag.trim());
+            } else {
+                newNode.tags = profile.chips?.split(',').map(tag => tag.trim());                
+            }
+        }
+        newNode.cast = profile.cast.map(cast => cast.name);
+        newNode.creator = profile.Creator?.map(creator => creator.name);
+        newNode.stars = profile.Stars?.map(star => star.name);
 
-    // if (profile.type === 'title') {
-    //     let titleNode = findNodeByProperty(cy, 'label', profile.name);
-    //     if (!titleNode) {
-    //         titleNode = createNode(cy, profile.name);
-    //         titleNode.data('url', message.imdbUrl);
-    //         titleNode.data('type', profile.type);
-            
-    //         titleNode.data('subtype', `imdb${profile.subtype}`);
-    //         titleNode.data('shape', 'square');
-    //         newNodes = newNodes.union(titleNode);
-    //     }
-    //     titleNode.data('rating', profile.rating.split('/')[0]);
-    //     if (profile.image) titleNode.data('image', profile.image);
-    //     if (profile.plot) titleNode.data('plot', profile.plot);
-    //     if (profile.period) titleNode.data('period', profile.period);
-    //     if (profile.chips) titleNode.data('tags', profile.chips);
-    //     if (profile.Director) titleNode.data('director', profile.Director.reduce((acc, director) => acc + ', ' + director.name + ', ', ''));
-    //     if (profile.Writers) titleNode.data('writers', profile.Writers.reduce((acc, writer) => acc + ', ' + writer.name + ', ', ''));
-    //     if (profile.Creators) titleNode.data('creators', profile.Creators.reduce((acc, creator) => acc + ', ' + creator.name + ', ', ''));
-    //     if (profile.cast) {
-    //         profile.cast.forEach(actor => {
-    //             let actorNode = findNodeByProperty(cy, 'label', actor.name);
-    //             if (!actorNode) {
-    //                 actorNode = createNode(cy, actor.name);
-    //                 actorNode.data('url', actor.actorUrl);
-    //                 actorNode.data('type', 'person');
-    //                 actorNode.data('subtype', `actor`);
-    //                 actorNode.data('shape', 'star');
-    //                 newNodes = newNodes.union(actorNode);
-    //             }
-    //             if (actor.image) actorNode.data('image', actor.image);
-    //             const edge = createEdge(cy, actorNode, titleNode);
-    //             edge.data('label', 'acted in');
-    //             edge.data('type', 'actedIn');
-    //             edge.data('role', actor.characterName);
-    //             edge.data('details', actor.details);
-    //         });
-    //     }
+        if (profile.moreLikeThis) {
+            newNode.moreLikeThis = profile.moreLikeThis.map(item => item.title);
+            newNode.moreLikeThisArray = profile.moreLikeThis
+        }
+    }
 
-    //     // personNode.data('about', profile.about);
-    //     // if (profile.location) personNode.data('location', profile.location);
-
-    // }
-    //         // run layout for new nodes
-    //         newNodes.layout({
-    //             name: 'random',
-    //             animate: true,
-    //             animateFilter: function (node, i) {
-    //                 return true;
-    //             },
-    //             animationDuration: 1000,
-    //             animationEasing: undefined,
-    //             fit: true,
-    //         })
-    //             .run();
+    return newNode
 }
+
+
+/* Creator
+: 
+[{…}]
+Stars
+: 
+(3) [{…}, {…}, {…}]
+cast
+: 
+(18) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+chips
+: 
+(9) ['Conspiracy Thriller', 'Dystopian Sci-Fi', 'Psychological Drama', 'Psychological Thriller', 'Workplace Drama', 'Drama', 'Mystery', 'Sci-Fi', 'Thriller']
+image
+: 
+"https://m.media-amazon.com/images/M/MV5BZDI5YzJhODQtMzQyNy00YWNmLWIxMjUtNDBjNjA5YWRjMzExXkEyXkFqcGc@._V1_QL75_UX190_CR0,2,190,281_.jpg"
+name
+: 
+"Severance"
+period
+: 
+"TV Series"
+plot
+: 
+"Mark leads a team of office workers whose memories have been surgically divided between their work and personal lives. When a mysterious colleague appears outside of work, it begins a journey to discover the truth about their jobs."
+rating
+: 
+"8.7/10261K"
+subtype
+: 
+"TV Series"
+type
+: 
+"title"
+*/
